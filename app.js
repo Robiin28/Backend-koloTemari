ae5f73613e88b9ae75b0a991866b47bbc5de55f5
Robel <155951971+Robiin28@users.noreply.github.com>
2025-09-25 20:39:28 +0300

    adding vercel domain

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'https://kolootemari.vercel.app'
+        ' https://frontend-kolotemari.vercel.app/'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
