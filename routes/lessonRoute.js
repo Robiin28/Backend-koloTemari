e755ca110c90f929e15aefaa8c3ae2e0ceef017e
Robel <155951971+Robiin28@users.noreply.github.com>
2025-09-27 14:22:00 +0300

    Update lessonRoute.js

--- routes/lessonRoute.js
+++ routes/lessonRoute.js
@@ -7,7 +7,7 @@ const validateCourseId = require('../middleware/validateCourseId'); // Import th
 const router = express.Router({ mergeParams: true }); // Ensure mergeParams is set to true
 
 // Middleware to protect routes
-// router.use(authController.protect);
+router.use(authController.protect);
 
 // Middleware to validate course ID for all lesson routes under /api/courses/:courseId/lessons
 router.use(validateCourseId);
