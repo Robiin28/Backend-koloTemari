559f770d18d1cfb000fd1d3fa483b09811d24d55
Robel <155951971+Robiin28@users.noreply.github.com>
2025-10-05 18:29:22 +0300

    Update passport.js
    checking again

--- utils/passport.js
+++ utils/passport.js
@@ -1,7 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
 const User = require('./../models/UserModel');
 
+// ---------------- Google ----------------
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -9,22 +11,15 @@ passport.use(new GoogleStrategy({
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
-      // Check if user already exists (by googleId or email)
       let user = await User.findOne({ googleId: profile.id });
+      if (!user) user = await User.findOne({ email: profile.emails[0].value });
       if (!user) {
-        // If not found by googleId, try email
-        user = await User.findOne({ email: profile.emails[0].value });
-      }
-      if (!user) {
-        // Create new user consistent with your signup pattern
-        user = new User({
+        user = await User.create({
           googleId: profile.id,
           name: profile.displayName,
           email: profile.emails[0].value,
-          active: true   // Since user is verified by Google
-          // Add other fields as per your user schema
+          active: true
         });
-        await user.save();
       }
       done(null, user);
     } catch (err) {
@@ -33,8 +28,34 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+// ---------------- GitHub ----------------
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_REDIRECT_URI
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await User.findOne({ githubId: profile.id });
+      if (!user) user = await User.findOne({ email: profile.emails[0].value });
+      if (!user) {
+        user = await User.create({
+          githubId: profile.id,
+          name: profile.username,
+          email: profile.emails[0].value,
+          active: true
+        });
+      }
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  }
+));
+
+// ---------------- serialize / deserialize ----------------
 passport.serializeUser((user, done) => done(null, user.id));
-passport.deserializeUser(async (id, done) => { 
-  const user = await User.findById(id); 
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
   done(null, user);
 });
